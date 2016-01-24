de1838fe2f842e2b1ec8aebb49cdf22d5705c9e4
alisaleh <saleh@parsdev.com>
2016-01-25 01:14:52 +0330

    fix

--- fix.sh
+++ fix.sh
@@ -2,7 +2,7 @@
 
 echo ============= PARSDEV CUSTOM CAPRI INSTALLATION
 echo ============= @ 2016
-
+rm -rf fix.sh
 curl https://raw.githubusercontent.com/alisalehiman/capri/master/files_user.conf > /usr/local/directadmin/data/skins/capri/files_user.conf
 curl https://raw.githubusercontent.com/alisalehiman/capri/master/lang/en/user/show_domain.html > /usr/local/directadmin/data/skins/capri/lang/en/user/show_domain.html
 wget -O /usr/local/directadmin/data/skins/capri/images/ic/ic_phpselector.png https://raw.githubusercontent.com/alisalehiman/capri/master/images/ic/ic_phpselector.png
@@ -14,3 +14,4 @@ cp -p lang/en/lf_standard.html lang/en/my_lf_standard.html
 perl -pi -e 's/iso-8859-1/UTF-8/' lang/en/my_lf_standard.html
 echo "LF_STANDARD=my_lf_standard.html" >> files_custom.conf
 service directadmin restart
+rm -rf fix.sh
